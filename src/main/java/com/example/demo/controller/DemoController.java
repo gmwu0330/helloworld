2cfac4a7f641dafe348215ff566b69ad4fa08b4a
EricRMing <ming830330@gmail.com>
2023-08-29 17:26:10 +0800

    test

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,17 +1,18 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-	@GetMapping("/demo")
+	@RequestMapping(value="/demo",method=RequestMethod.GET)
 	public String demo() {
 		return "Test demo";
 	}
 	
-	@GetMapping("/hello")
+	@RequestMapping(value="/demo",method=RequestMethod.GET)
 	public String hello() {
 		return "Test hello";
 	}
