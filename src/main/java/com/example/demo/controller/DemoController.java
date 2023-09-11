6c9e7e396b6d0e273a8d19def3b0271e663be6ab
EricRMing <ming830330@gmail.com>
2023-09-11 17:48:39 +0800

    3

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -9,7 +9,7 @@ public class DemoController {
 
 	@RequestMapping(value="/demo",method=RequestMethod.GET)
 	public String demo() {
-		return "Test demo test2";
+		return "Test demo test3";
 	}
 	
 	@RequestMapping(value="/hello",method=RequestMethod.GET)
